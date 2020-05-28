3762d5c999e0cd08e5a4af18677c5b81a3f57693
carter.hesterman <carter.hesterman@lifeomic.com>
2020-05-28 14:22:56 -0600

    init

---
+++ terraform/lambda.tf
@@ -0,0 +1,8 @@
+module "timed_lambda" {
+  source = "../../../../hesto2-terraform-modules/timed_lambda"
+  app_name = "owlet-worker"
+  lambda_environment_variables = {
+    NODE_ENV = "production"
+  }
+  schedule_expression = "rate(5 minutes)"
+}
