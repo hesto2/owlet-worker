b4095036021cb3d89433ab52547564c3ed09b25a
carter.hesterman <carter.hesterman@lifeomic.com>
2020-05-28 19:43:58 -0600

    add logging

--- src/checkBaseStation.ts
+++ src/checkBaseStation.ts
@@ -9,16 +9,20 @@ const notificationUrl = process.env.NOTIFICATION_URL;
 
 const checkBaseStation = async () => {
   try {
+    console.log('getting token');
     const token = await getAuthToken();
     const devices = await getDevices(token);
     const sock = await getDeviceAsSmartSock(devices[0].device.dsn, token);
+    console.log('got sock');
     if (sock.chargeStatus === 0) {
+      console.log('turning on base station');
       await setBaseStationOn(true, sock, token);
       await axios.post(notificationUrl, {
         channel: 'mini-teammate',
         message: `Turned on the base station automatically`,
       });
     }
+    console.log('done');
   } catch (err) {
     console.error(err);
     if (notificationUrl) {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Context } from 'aws-lambda';
 import checkBaseStation from './checkBaseStation';
 export const handler = async (_event: unknown, _context: Context) => {
-  checkBaseStation();
+  await checkBaseStation();
 };
